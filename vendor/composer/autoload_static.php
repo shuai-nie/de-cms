7c9ca3409fb7c7a26cb7f9fc269f99d39bc2531a
Lucius <yesheng35@126.com>
2021-10-14 16:35:37 +0800

    日志

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -13,6 +13,7 @@ class ComposerStaticInitacb7b6ac39ac135c331d42651ad45f97
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+        '1cfd2761b63b0a29ed23657ea394cb2d' => __DIR__ . '/..' . '/topthink/think-captcha/src/helper.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -20,6 +21,7 @@ class ComposerStaticInitacb7b6ac39ac135c331d42651ad45f97
         array (
             'think\\view\\driver\\' => 18,
             'think\\trace\\' => 12,
+            'think\\captcha\\' => 14,
             'think\\app\\' => 10,
             'think\\' => 6,
         ),
@@ -56,6 +58,10 @@ class ComposerStaticInitacb7b6ac39ac135c331d42651ad45f97
         'think\\trace\\' => 
         array (
             0 => __DIR__ . '/..' . '/topthink/think-trace/src',
+        ),
+        'think\\captcha\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-captcha/src',
         ),
         'think\\app\\' => 
         array (
