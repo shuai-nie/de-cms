888a5c8f3d1983905ffd29c9316394ff10e73583
nie <yesheng35@126.com>
2021-10-25 23:05:50 +0800

    123

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
 
     // 应用映射（自动多应用模式有效）
     'app_map'          => [
-        'admin' => 'Admin'
+//        'admin' => 'Admin'
     ],
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [],
