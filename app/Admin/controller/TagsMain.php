bea64da85e965a99862929fa97f49d9b9f1aaa9a
Lucius <yesheng35@126.com>
2021-10-26 13:45:44 +0800

    更新

--- app/Admin/controller/TagsMain.php
+++ app/Admin/controller/TagsMain.php
@@ -3,6 +3,10 @@ declare (strict_types = 1);
 
 namespace app\Admin\controller;
 
+use app\Admin\model\Archives;
+use app\Admin\model\Tagindex;
+use app\Admin\model\Taglist;
+use think\facade\Db;
 use think\Request;
 use think\facade\View;
 use app\Admin\model\Flink as FlinkModel;
@@ -30,79 +34,114 @@ class TagsMain extends Base
      */
     public function index()
     {
-        $data = FlinkModel::where(array())->select()->toArray();
-        foreach ($data as $k=>$v){
-            if(!empty($v['logo'])){
-                $v['logo'] = '<img src="'.$v['logo'].'" width="40" height="40" />';
-            }else{
-                $v['logo'] = '无图标';
-            }
-            $data[$k] = $v;
-        }
+
+        $offset = 0;
+        $length = 20;
+        $data = Tagindex::where("")->order('id desc')->paginate($length);
         View::assign('_data', $data);
         return View::fetch();
-
     }
 
-
-    public function create()
+    public function tags_main_fetch()
     {
+        $timestamp = time();
+        $wheresql = '';
+        $start = isset($start) && is_numeric($start) ? $start : 0;
+        $where = array();
+        if(isset($startaid) && is_numeric($startaid) && $startaid > 0){
+            $where[] = " id>$startaid ";
+        }else{
+            $startaid = 0;
+        }
+
+        if(isset($endaid) && is_numeric($endaid) && $endaid > 0){
+            $where[] = " id<$endaid ";
+        }else{
+            $endaid = 0;
+        }
+        if(!empty($where))
+        {
+            $wheresql = " arcrank>-1 AND ".implode(' AND ', $where);
+        }
+//        $query = "SELECT id as aid,arcrank,typeid,keywords FROM `#@__archives` $wheresql LIMIT $start, 100";
+//        $dsql->SetQuery($query);
+//        $dsql->Execute();
+//        $complete = true;
+//        while($row = $dsql->GetArray())
+        $row = Archives::where($wheresql)->limit($start, 100)->select();
+        foreach($row as $k => $vo){
+            $aid = $vo['aid'];
+            $typeid = $vo['typeid'];
+            $arcrank = $vo['arcrank'];
+            $vo['keywords'] = trim($vo['keywords']);
+            if($vo['keywords']!='' && !preg_match("#,#", $vo['keywords'])){
+                $keyarr = explode(' ', $vo['keywords']);
+            }else{
+                $keyarr = explode(',', $vo['keywords']);
+            }
+
+            foreach($keyarr as $keyword)
+            {
+                $keyword = trim($keyword);
+                if($keyword != '' && strlen($keyword)<13 )
+                {
+                    $keyword = addslashes($keyword);
+                    //$row = $dsql->GetOne("SELECT id FROM `#@__tagindex` WHERE tag LIKE '$keyword'");
+                    $row = Tagindex::where('tag', 'like', '%'.$keyword.'%')->find();
+                    if(is_array($row))
+                    {
+                        $tid = $row['id'];
+//                        $query = "UPDATE `#@__tagindex` SET `total`=`total`+1 WHERE id='$tid' ";
+                        Tagindex::where("id=".$tid)->inc('total')->update();
+//                        $dsql->ExecuteNoneQuery($query);
+                    }
+                    else
+                    {
+//                        $query = " INSERT INTO `#@__tagindex`(`tag`,`count`,`total`,`weekcc`,`monthcc`,`weekup`,`monthup`,`addtime`) VALUES('$keyword','0','1','0','0','$timestamp','$timestamp','$timestamp');";
+//                        $dsql->ExecuteNoneQuery($query);
+                        $tid = Tagindex::Insert(array(
+                            'tag'     => $keyword,
+                            'count'   => 0,
+                            'total'   => 1,
+                            'weekcc'  => 0,
+                            'monthcc' => 0,
+                            'weekup'  => $timestamp,
+                            'monthup' => $timestamp,
+                            'addtime' => $timestamp,
+                        ), true);
+//                        $tid = $dsql->GetLastID();
+                    }
+//                    $query = "REPLACE INTO `#@__taglist`(`tid`,`aid`,`typeid`,`arcrank`,`tag`) VALUES ('$tid', '$aid', '$typeid','$arcrank','$keyword'); ";
+//                    $dsql->ExecuteNoneQuery($query);
+                    Taglist::replace()->insert(array(
+                        'tid'     => $tid,
+                        'aid'     => $aid,
+                        'typeid'  => $typeid,
+                        'arcrank' => $arcrank,
+                        'tag'     => $keyword,
+                    ), true);
+
+                }
+            }
+            $complete = FALSE;
+        }
+        if($complete)
+        {
+            return json(['code'=>0, 'msg'=>'成功']);
+        }
+        return json(['code'=>1, 'msg'=>'失败']);
 
     }
 
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
 
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
 
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
+
+
+
+
+
+
+
 }
---
+++ app/Admin/model/Tagindex.php
@@ -0,0 +1,14 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\Admin\model;
+
+use think\Model;
+
+/**
+ * @mixin \think\Model
+ */
+class Tagindex extends Model
+{
+    //
+}
