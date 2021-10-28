66cce1b658a003358eb0e241e209d1cae8213037
Lucius <yesheng35@126.com>
2021-10-28 17:03:39 +0800

    自定义文档属性完成

--- app/Admin/controller/ContentAtt.php
+++ app/Admin/controller/ContentAtt.php
@@ -3,7 +3,7 @@ declare (strict_types = 1);
 
 namespace app\Admin\controller;
 
-use think\Request;
+use think\facade\Request;
 use think\facade\View;
 use app\Admin\model\Arcatt as ArcattModel;
 
@@ -30,6 +30,27 @@ class ContentAtt extends Base
      */
     public function index()
     {
+        if(Request::isPost()){
+            $param = Request::param('');
+
+            $startID = 1;
+            $endID = $param['idend'];
+            for(; $startID<=$endID; $startID++)
+            {
+                $att = $param['att_'.$startID];
+                $attname = $param['attname_'.$startID];
+                $sortid = $param['sortid_'.$startID];
+                ArcattModel::update(array(
+                    'attname' => $attname,
+                    'sortid' => $sortid,
+                ), array(
+                    'att' => $att
+                ));
+            }
+
+            return $this->success("修改成功", (string)url('index'));
+
+        }
         $length = 20;
         $map = array();
         $data = ArcattModel::where($map)->order('sortid asc')->paginate($length);
@@ -37,69 +58,14 @@ class ContentAtt extends Base
         return View::fetch();
     }
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
 
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
 
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
+
+
+
+
+
 }
