dc60d0a9e28c0c45fb52409a136d7f990069bcbb
Lucius <yesheng35@126.com>
2021-11-04 16:30:31 +0800

    123

--- app/Admin/controller/FriendlinkMain.php
+++ app/Admin/controller/FriendlinkMain.php
@@ -3,11 +3,11 @@ declare (strict_types = 1);
 
 namespace app\Admin\controller;
 
-use app\Admin\model\Flink;
-use app\Admin\model\Flinktype;
 use think\facade\Request;
 use think\facade\View;
-use app\Admin\model\Flink as FlinkModel;
+use app\Admin\model\Flink;
+use app\Admin\model\Flinktype;
+use think\Image;
 
 /**
  * [有情链接]
@@ -32,7 +32,7 @@ class FriendlinkMain extends Base
      */
     public function index()
     {
-        $data = FlinkModel::where([])->select();
+        $data = Flink::where([])->select();
         View::assign('_data', $data);
         return View::fetch();
     }
@@ -45,14 +45,14 @@ class FriendlinkMain extends Base
 
             $state = Flink::insert(array(
                 'sortrank' => $param['sortrank'],
-                'url' => $param['url'],
-                'webname' => $param['webname'],
-                'logo' => $param['logo'],
-                'msg' => $param['msg'],
-                'email' => $param['email'],
-                'typeid' => $param['typeid'],
-                'dtime' => $dtime,
-                'ischeck' => $param['ischeck'],
+                'url'      => $param['url'],
+                'webname'  => $param['webname'],
+                'logo'     => $param['logo'],
+                'msg'      => $param['msg'],
+                'email'    => $param['email'],
+                'typeid'   => $param['typeid'],
+                'dtime'    => $dtime,
+                'ischeck'  => $param['ischeck'],
             ));
             if($state !== false){
                 return $this->success('提交成功', (string)url('index'));
@@ -93,13 +93,13 @@ class FriendlinkMain extends Base
 
             $state = Flink::update(array(
                 'sortrank' => $param['sortrank'],
-                'url' => $param['url'],
-                'webname' => $param['webname'],
-                'logo' => $param['logo'],
-                'msg' => $param['msg'],
-                'email' => $param['email'],
-                'typeid' => $param['typeid'],
-                'ischeck' => $param['ischeck']
+                'url'      => $param['url'],
+                'webname'  => $param['webname'],
+                'logo'     => $param['logo'],
+                'msg'      => $param['msg'],
+                'email'    => $param['email'],
+                'typeid'   => $param['typeid'],
+                'ischeck'  => $param['ischeck'],
             ), array(
                 'id' => $param['id']
             ));
@@ -126,6 +126,39 @@ class FriendlinkMain extends Base
         View::assign('row', $row);
         return View::fetch();
     }
+
+    /**
+     * [水印案例]
+     * @author Dave 178698695@qq.com
+     */
+    public function src()
+    {
+        $root = root_path('public\storage');
+        $image = Image::open($root.'image/20211104/a953b733224457e06c44da0b0b765bb8.jpg');
+        $width = $image->width();
+        $height = $image->height();
+        $type = $image->type();
+        $mime = $image->mime();
+        $size = $image->size();
+
+        // 后台剪裁
+        //$image->crop(100, 100, 50, 20)->save($root.time().'.png');
+
+        // 生成缩略图
+        //$image->thumb(50, 50, Image::THUMB_CENTER)->save($root.time().'_ss'.'.png');
+
+        // 图片水印
+        //$image->water($root.'pview.gif', Image::FLIP_X, 50)->save($root.time().'_water_img.png');
+
+        // 文字水印
+        $image->text(time().'十年磨一剑', $root.'msyhl.ttc', 20, '#f44336', Image::WATER_CENTER, 00, 50)->save($root.time().'text_image.png');
+
+
+
+
+    }
+
+
 
 
 
