f9c2be5a669c2cfbb54859a5c59c305e2a7d203b
nie <yesheng35@126.com>
2021-11-28 16:52:59 +0800

    123

--- app/Admin/controller/CatalogMain.php
+++ app/Admin/controller/CatalogMain.php
@@ -463,7 +463,8 @@ class CatalogMain extends Base
         // 根目录
         $cfg_basedir = app()->getRootPath().'view';
         $basehost = 'http://'.$_SERVER['HTTP_HOST'];
-        $cfg_templets_dir = '/templets';
+        //$cfg_templets_dir = '/templets';
+        $cfg_templets_dir = '/';
         View::assign('cfg_templets_dir', $cfg_templets_dir);
         View::assign('cfg_basedir', $cfg_basedir);
         View::assign('activepath', $activepath);
