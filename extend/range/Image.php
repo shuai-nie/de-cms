354c6a19862062487a36e0ed9feb08354e052f91
Lucius <yesheng35@126.com>
2022-02-18 11:39:51 +0800

    添加远程图片下载

--- extend/range/Image.php
+++ extend/range/Image.php
@@ -1,51 +1,22 @@
 <?php
-declare (strict_types = 1);
+namespace range;
 
-namespace app\Admin\controller;
+/**
+ * 远程图片下载到本地
+ * Class Image
+ * @author Lucius yesheng35@126.com
+ * @package range
+ */
+class Image {
 
-use think\Request;
-use Snoopy\Snoopy as SnoopyClass;
-
-class Snoopy
-{
     /**
-     * 显示资源列表
-     * @return \think\Response
+     * @param $imgUrl
+     * @return \think\response\Json
+     * @author Lucius yesheng35@126.com
      */
-    public function index()
+    public function RangeImage($imgUrl)
     {
-        $snoopy = new SnoopyClass();
-        $snoopy = new \Snoopy\Snoopy();
-        $url = "https://www.milu.com/sysjttwzc/";
-        $snoopy->fetch($url);
-        $results = $snoopy->results;
-
-        $regex4 = "/<div class=\"mainLeft globalSectionContainer\".*?>.*?<\/div>/ism";
-
-        preg_match_all($regex4, $results, $matches);
-        var_dump($matches);
-        exit();
-
-        $preg = '/<a .*?href="(.*?)".*?>/is';
-        preg_match_all($preg, $str, $array2);
-        for ($i = 0; $i < count($array2[1]); $i++) {
-            echo $array2[1][$i] . "<br />";
-        }
-
-        /*$pattern = "/[img|IMG].*?src=['|\"](.*?(?:[.gif|.jpg]))['|\"].*?[\/]?>/";
-        preg_match_all($pattern, $results, $match);
-        var_dump($match[1]);*/
-
-
-
-
-
-    }
-
-
-    public function cc()
-    {
-        $imgUrl = "https://static.app.985sy.com/attachment/syapp/logo/202202091644376607.jpg";
+        //$imgUrl = "https://static.app.985sy.com/attachment/syapp/logo/202202091644376607.jpg";
         // 文件保存目录路径
         $save_path = $_SERVER ['DOCUMENT_ROOT'] . '/storage/image/';
         // 文件保存目录URL
@@ -75,13 +46,11 @@ class Snoopy
         return json(['imgurl' => $imgUrl, 'url' => $this->get_http_type() . $_SERVER['HTTP_HOST'] . $saveUrl . '/' . $imgname]);
     }
 
-
     public function get_http_type()
     {
         $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
         return $http_type;
     }
-
 
 
 
